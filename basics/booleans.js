dd5aece963e1e01ec195322fd0be7b5e81a6c40f
ian <ianhouska@gmail.com>
2026-07-02 22:58:04 -0500

    changed booleans

--- basics/booleans.js
+++ basics/booleans.js
@@ -18,25 +18,3 @@ console.log(!isOpen);
 // 1. Declare two boolean variables.
 // 2. Log the results of &&, ||, and !.
 // 3. Use the values inside an if statement.
-let trueValue1 = true;
-let falseValue1 = false;
-console.log(trueValue1 && falseValue1);
-console.log(trueValue1 || falseValue1);
-console.log(!trueValue1);
-
-
-
-// Answer: booleans
-// Example solution:
-const isLoggedIn = true;
-const hasPermission = false;
-console.log('and:', isLoggedIn && hasPermission);
-console.log('or:', isLoggedIn || hasPermission);
-console.log('not:', !isLoggedIn);
-if (isLoggedIn && !hasPermission) {
-  console.log('Logged in but no permission');
-} else if (isLoggedIn && hasPermission) {
-  console.log('Logged in with permission');
-} else {
-  console.log('Not logged in');
-}
